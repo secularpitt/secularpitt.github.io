b0be31146425c8b65d71fa4283546179c364a410
Noah Scholfield <njscholfield@users.noreply.github.com>
2017-02-05 15:27:54 -0500

    Switch to browser form submission

--- js/form.js
+++ js/form.js
@@ -1,33 +1,21 @@
 (function() {
   $(document).ready(function() {
 
-    $('form').submit(function(e) {
-      e.preventDefault();
-      var formData = {
-        'entry.748603620': $('input[name=name]').val(),
-        'entry.1018829523': $('input[name=email]').val(),
-        'entry.2042971329': $('textarea[name=message]').val(),
-        'entry.214764784': ($('input[name=email-list]').prop('checked')) ? 'Yes' : ''
-      };
-
-      $.ajax({
-        type: 'POST',
-        url: 'https://docs.google.com/forms/d/e/1FAIpQLScYLomzA21CSZ2IQo8Ls5tSKccgpvUOofqNlSSOEVF_ksZp4w/formResponse',
-        data: formData,
-        dataType: 'html',
-        encode: true
-      }).done(function(response) {
-        console.log("Success");
-        var h = $parseHTML(response);
-        var message = $('.freebirdFormviewerViewResponseConfirmationMessage').HTML(h);
-        console.log(message);
-        $('#success-alert').removeClass('hidden');
-        $('form').trigger("reset");
-      }).fail(function(xhr, status, error) {
-        console.log('Failure');
-        console.log(status);
-        console.log(error);
-      });
+    $('input[name=email-list]').click(function(e) {
+      var list = ($('input[name=email-list]').prop('checked')) ? 'Yes' : '';
+      $('input[name="entry.214764784"]').val(list);
     });
-  })
+
+    $('form').submit(function(e) {
+      window.setTimeout(function() {
+        var iframe = $('iframe');
+        if(iframe.length) {
+          $('#success-alert').removeClass('hidden');
+          $('form').trigger('reset');
+        } else {
+          $('#failure-alert').removeClass('hidden');
+        }
+      }, 1000);
+    });
+  });
 })();
