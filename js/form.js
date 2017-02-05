9d1909b2dd7a973140172c5d201f2e13884afca8
Noah Scholfield <njscholfield@users.noreply.github.com>
2017-02-05 15:49:02 -0500

    Check if iframe is loaded

--- js/form.js
+++ js/form.js
@@ -1,21 +1,25 @@
 (function() {
   $(document).ready(function() {
-
+    var iframeLoaded = false;
     $('input[name=email-list]').click(function(e) {
       var list = ($('input[name=email-list]').prop('checked')) ? 'Yes' : '';
       $('input[name="entry.214764784"]').val(list);
     });
 
     $('form').submit(function(e) {
+      iframeLoaded = false;
       window.setTimeout(function() {
-        var iframe = $('iframe');
-        if(iframe.length) {
+        if(iframeLoaded) {
           $('#success-alert').removeClass('hidden');
           $('form').trigger('reset');
         } else {
           $('#failure-alert').removeClass('hidden');
         }
       }, 1000);
+    });
+    
+    $('iframe').on('load', function() {
+      iframeLoaded = true;
     });
   });
 })();
