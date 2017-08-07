da755817847ca343bce4075dbc04912e683f8214
Noah Scholfield <contact@noahscholfield.com>
2017-08-07 19:38:59 -0400

    Add mailing list signup for activities fair

---
+++ contact/mailing-list/mailchimp.js
@@ -0,0 +1,47 @@
+(function() {
+  const form = document.querySelector('form');
+  const email = document.querySelector('input[name="email"]');
+  const firstName = document.querySelector('input[name="FNAME"]');
+  const lastName = document.querySelector('input[name="LNAME"]');
+  const errorMess = document.querySelector('#errormess');
+  const successMess = document.querySelector('#successmess');
+
+  function subscribe(e) {
+    e.preventDefault();
+    clearMessages();
+    var newSubscriber = {
+      'email_address': email.value,
+      'status': 'subscribed',
+      'merge_fields': {
+        'FNAME': firstName.value,
+        'LNAME': lastName.value
+      }
+    };
+    var fetchConfig = {
+      method: 'POST',
+      headers: new Headers({'Content-Type': 'application/json'}),
+      body: JSON.stringify(newSubscriber)
+    };
+    fetch('https://fbmb.noahscholfield.com/api/mailchimp/', fetchConfig)
+      .then(blob => blob.json())
+      .then(data => {
+        if(data.success) {
+          successMess.innerHTML = data.message;
+          form.reset();
+        } else {
+          errorMess.innerHTML = data.message;
+        }
+      })
+      .catch((err) => {
+        errorMess.innerHTML = 'Something is wrong. Try again!';
+        console.log('Error', err);
+      });
+  }
+
+  function clearMessages() {
+    errorMess.innerHTML = '';
+    successMess.innerHTML = '';
+  }
+
+  form.addEventListener('submit', subscribe);
+})();
