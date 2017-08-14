35b6f28426862dd126ad6d16b11a4c6ca4e14ca0
Noah Scholfield <contact@noahscholfield.com>
2017-08-14 15:04:01 -0400

    Pull events from Google Calendar

---
+++ js/events.js
@@ -0,0 +1,43 @@
+/* global Vue: false, moment: false */
+const now = new Date();
+const calendarID = 'secularpitt@gmail.com';
+const numOfResults = 10;
+const API_KEY = 'AIzaSyBILdBYibAQSy_7LMncr40hmLOLgDj4XFI';
+
+const calURL = `https://www.googleapis.com/calendar/v3/calendars/${calendarID}/events?maxResults=${numOfResults}&orderBy=startTime&singleEvents=true&timeMin=${now.toISOString()}&key=${API_KEY}`;
+
+var calendar = new Vue({
+  el: '#calendar',
+  data: {
+    title: 'Test',
+    events: [],
+    now: Date.now()
+  },
+  methods: {
+    getCalendarEvents() {
+      fetch(calURL)
+        .then(blob => blob.json())
+        .then(data => this.events = data.items)
+        .then(() => this.events.forEach(item => {
+          item.description = (item.description) ? item.description.split('--'): ['',''];
+        }))
+        .catch((response) => console.log('Error', response));
+    }
+  },
+  mounted() {
+    this.getCalendarEvents();
+  },
+  filters: {
+    date(value) {
+      if(value.date) {
+        return moment(value.date).format('dddd, M/D');
+      } else {
+        return moment(value.dateTime).format('dddd, M/D @ ha');
+      }
+    },
+    id(value) {
+      const startDate = (value.date) ? value.date: value.dateTime;
+      return moment(startDate).format('MM-DD-YYYY');
+    }
+  }
+});
