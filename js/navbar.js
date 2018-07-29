b921ab3b6892537c19b3a3ae1a50fecc0862bbaf
Noah Scholfield <contact@noahscholfield.com>
2018-07-29 13:06:09 -0400

    Less reliance on JavaScript

--- js/navbar.js
+++ js/navbar.js
@@ -2,15 +2,11 @@
 var navbar = new Vue({
   el: '#navbar',
   data: {
-    page: location.pathname.slice(1,-1),
     collapsed: true,
   },
   methods: {
     toggleCollapsed() {
       this.collapsed = !this.collapsed;
-    },
-    isCurrentPage(pageName) {
-      return pageName === this.page;
     }
   }
 });
